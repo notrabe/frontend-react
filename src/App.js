b9952e0d31033a0f06dbb83bcfb486657d01c1fa
crystal-csete <crystal-csete@lambdastudents.com>
2020-10-19 16:30:49 -0600

    added a route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import Login from './componets/Login';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Signin from './componets/Signin';
+import './App.css'
 
+class App extends Component {
 
-
-function App() {
- 
-  
+  render() {
   return (
-   <div>
 
-     <Login  />
 
-   </div>
-  );
+
+      <Router>
+        <div className="App">
+          <h1>Home Page</h1>
+          <Switch>
+            <Route path='/signin' component={Signin}/>
+          </Switch>
+        </div>
+      </Router>
+
+  )
+}
 }
 
-export default App;
+
+export default App

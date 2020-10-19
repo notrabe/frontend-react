644ed4c5f18a05387ad17effd6a0f2fd6551e059
crystal-csete <crystal-csete@lambdastudents.com>
2020-10-19 11:43:38 -0600

    added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Login from './componets/Login';
+import './App.css';
+
+
+
+function App() {
+ 
+  
+  return (
+   <div>
+
+     <Login  />
+
+   </div>
+  );
+}
+
+export default App;
---
+++ src/componets/Login.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
+
+
+
+function Login() {
+  
+  const {register, handleSubmit, errors} = useForm();
+  const [dropDown, setDropDown] = useState("Operator");
+  
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
+
+  return (
+      <div className="container-logo">
+        <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTx-1_0MUCoeqLDVJog4Gbfbh1vyFAtldTT4A&usqp=CAU" alt="" />
+      <h1>Log In Here</h1>
+    <form className="forms-container" onSubmit={handleSubmit(onSubmit)} >
+      <input  type="username" placeholder="Username" name="username" ref={register({required: "USERNAME IS REQUIRED", minLength: {value: 8, message:"TOO SHORT" }})} />
+      {errors.username && <p>Username is invalid</p>}
+
+      <input type="text" placeholder="Email" name="email" ref={register}/>
+
+      <input type="password" placeholder="Password" name="password" ref={register({required: "PASSWORD IS REQUIRED", minLength: {value: 8, message:"TOO SHORT" }})}/>
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <input type="location" placeholder="Location" name="location" ref={register({required: "LOCATION IS REQUIRED", minLength: {value: 2, message:"TOO SHORT" }})}/>
+      {errors.location && <p>{errors.location.message}</p>}
+
+      <input type="submit" />
+ 
+      
+      
+      {/* DROPDOWN FOR DINER OR OPERATOR HERE */}
+        <h1>Choose To Get Started : {dropDown}</h1>
+        <select  value={dropDown} onChange={(e) => {setDropDown(e.target.value)}} >
+          <option value="Operator">Operator</option>
+          <option value="Diner">Diner</option>
+        </select>
+
+
+
+    </form>
+    </div>
+  );
+}
+
+export default Login;

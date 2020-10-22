580760b7308700b0d96ae0226f7e3cf0688c3020
Evan Barton <notrabe1011@gmail.com>
2020-10-22 12:34:28 -0400

    added redirect on signin, added redirect on signup, tweaked review forms and added alert, added logout button

--- foodtruck-trackr/src/components/OperatorDashboard.js
+++ foodtruck-trackr/src/components/OperatorDashboard.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Route, NavLink, Switch, useHistory } from 'react-router-dom'
 import {TruckProvider} from '../context/TruckContext'
 import DinerDashboard from './DinerDashboard'
 import AddTruck from './AddTruck'
 import TruckList from './TruckList'
 import AddMenuItem from './AddMenuItem';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import axios from 'axios'
 
 function OperatorDashboard (){
+    
+    const history = useHistory()
+    
+    const handleLogOut = (e) =>{
+        e.preventDefault()
+        history.push('/')
+    }
+    
+    // useEffect(()=>{
+    //     axios
+    //         .get(`https://bw-foodtruck-tracker.herokuapp.com/api/users`)
+    //         .then(res=>{
+    //             console.log(res)
+    //             localStorage.getItem('token')
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //         })
+    //     }, [])
+
+
+
     return (
         <TruckProvider>    
             <div>
-                <NavLink to='/home'>Home</NavLink>
+                <button onClick={handleLogOut}>Log Out</button>
                 <h1>Trucks:</h1>
                 <AddTruck/>
                 <TruckList/>
